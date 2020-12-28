cc30a61f0027fe317f15f4fc64797997bba2f137
Qh92 <771600967@qq.com>
2020-12-28 23:19:20 +0800

    feat:添加泛型

--- generic/src/main/java/GenericTest3.java
+++ generic/src/main/java/GenericTest3.java
@@ -1,3 +1,5 @@
+import demo2.Person;
+import demo2.Student;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -75,8 +77,25 @@ public class GenericTest3 {
         List<?> list = null;
         list = list1;
         list = list2;
-        print(list1);
-        print(list2);
+        //编译通过
+        //print(list1);
+        //print(list2);
+
+        //
+        List<String> list3 = new ArrayList<>();
+        list3.add("AA");
+        list3.add("BB");
+        list3.add("CC");
+        list = list3;
+        //添加(写入)：对于List<?>就不能向其内部添加数据
+        //除了添加null之外
+        list.add(null);
+        //list.add("DD");
+        //list.add("?");
+        //获取(读取)：允许读取数据，读取的数据类型为Object
+        Object o = list.get(0);
+        System.out.println(o);
+
     }
 
     public void print(List<?> list){
@@ -85,6 +104,51 @@ public class GenericTest3 {
             Object next = iterator.next();
             System.out.println(next);
         }
+    }
+
+    /*
+    3.有限制条件的通配符的使用。
+       ? extends A : G<? extends A>可以作为G<A>和G<B>的父类，其中B是A的子类
+       ? super A : G<? super A>可以作为G<A>和G<B>的子类，其中B是A的父类
+     */
+    @Test
+    public void t4(){
+        List<? extends Person> list1 = null;
+        List<? super Person> list2 = null;
+
+        List<Student> list3 = new ArrayList<Student>();
+        List<Person> list4 = new ArrayList<Person>();
+        List<Object> list5 = new ArrayList<Object>();
+
+        list1 = list3;
+        list1 = list4;
+        //编译不通过
+        //list1 = list5;
+
+        //编译不通过
+        //list2 = list3;
+        list2 = list4;
+        list2 = list5;
+
+        //读取数据：
+        list1 = list4;
+        Person person = list1.get(0);
+        //编译不通过
+        //Student s = list1.get(0);
+
+        list2 = list4;
+        Object object = list2.get(0);
+        //编译不通过
+        //Person p = list2.get(0);
+
+        //写入数据
+        //编译不通过
+        //list1.add(new Student());
+        //list1.add(new Person());
+        //编译运行都可以
+        list2.add(new Person());
+        list2.add(new Student());
+
     }
 
 
