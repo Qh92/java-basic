d6d203ca99694d173db74cbda3ed846714cd4567
Qh92 <771600967@qq.com>
2021-01-05 23:12:54 +0800

    feat:新增读写操作

--- io/src/main/java/com/qinh/io/FileReaderWriterTest.java
+++ io/src/main/java/com/qinh/io/FileReaderWriterTest.java
@@ -2,10 +2,7 @@ package com.qinh.io;
 
 import org.junit.Test;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 /**
  * 一、流的分类
@@ -73,6 +70,73 @@ public class FileReaderWriterTest {
                 e.printStackTrace();
             }
         }
+    }
+
+    //对read()操作升级，使用read的重载方法
+    @Test
+    public void t2() {
+        FileReader fileReader = null;
+        try {
+            //1.File类的实例化
+            File file = new File("hello.txt");
+            //2.FileReader流的实例化
+            fileReader = new FileReader(file);
+            //3.读入的操作
+            //read(char[] cbuffer):返回每次读入cbuffer数组中的字符的个数。如果达到文件末尾，返回-1
+            char[] cbuffer = new char[5];
+            int len;
+            while ((len = fileReader.read(cbuffer)) != -1){
+                //错误的写法
+                /*for(int i = 0 ; i < cbuffer.length; i++){
+                    System.out.print(cbuffer[i]);
+                }*/
+                //正确写法
+                /*for(int i = 0 ; i < len; i++){
+                    System.out.print(cbuffer[i]);
+                }*/
+                //方式2
+                //错误写法
+                /*String s = new String(cbuffer);
+                System.out.print(s);*/
+                //正确写法
+                String s = new String(cbuffer, 0, len);
+                System.out.print(s);
+            };
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            //4.资源的关闭
+            try {
+                if(fileReader != null){
+                    fileReader.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /*
+    从内存中写出数据到硬盘的文件里
+    说明：
+    1.输出操作，对应的File可以不存在，并不会报异常
+    2.
+        File对应的硬盘中的文件，如果不存在，在输出的过程中，会自动创建此文件。
+        File对应的硬盘中的文件,如果存在，
+            如果流使用的构造器是：new FileWriter(file,false)/new FileWriter(file),对原有文件进行覆盖
+            如果流使用的构造器是：new FileWriter(file,true),不会对原有文件进行覆盖，而是在原有文件基础上追加内容
+     */
+    @Test
+    public void t3() throws IOException {
+        //1.提供File类的对象，指明写出到的文件
+        File file = new File("hello1.txt");
+        //2.提供FileWriter的对象，用于数据的写出
+        FileWriter fileWriter = new FileWriter(file,true);
+        //3.写出的操作
+        fileWriter.write("I hava a dream!\n");
+        fileWriter.write("you need to have a dream!");
+        //4.流资源的关闭
+        fileWriter.close();
 
     }
 
