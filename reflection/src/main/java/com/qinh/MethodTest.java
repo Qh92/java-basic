908c57e7b1a192428e8c902f40ab840d9e90d591
Qh92 <771600967@qq.com>
2021-02-01 23:57:19 +0800

    feat:添加反射获取方法等方法

--- reflection/src/main/java/com/qinh/MethodTest.java
+++ reflection/src/main/java/com/qinh/MethodTest.java
@@ -3,8 +3,10 @@ package com.qinh;
 import com.qinh.entity.Animal;
 import org.junit.Test;
 
-import javax.sound.midi.Soundbank;
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Objects;
 
 /**
  * 获取运行时类的方法结构
@@ -30,6 +32,77 @@ public class MethodTest {
         for (Method m : declaredMethods){
             System.out.println(m);
         }
+    }
+
+
+    /**
+     * @Xxx(注解)
+     * 权限修饰符  返回值类型  方法名(参数类型1 形参名1,...) throws XxxException{}
+     *
+     */
+    @Test
+    public void t2(){
+        Class<Animal> clazz = Animal.class;
+        Method[] declaredMethods = clazz.getDeclaredMethods();
+        for (Method m : declaredMethods){
+            //1.获取方法声明的注解
+            Annotation[] annotations = m.getAnnotations();
+            for (Annotation a : annotations){
+                //@com.qinh.entity.MyAnnotation(value=猫科)
+                System.out.print(a + "\t");
+            }
+            //2.权限修饰符
+            System.out.print(Modifier.toString(m.getModifiers()) + "\t");
+
+            //3.返回值类型
+            System.out.print(m.getReturnType().getName() + "\t");
+
+            //4.方法名
+            System.out.print(m.getName());
+
+            System.out.print("(");
+            //5.形参列表
+            Class[] parameterTypes = m.getParameterTypes();
+            if (!(Objects.isNull(parameterTypes) && parameterTypes.length == 0)){
+                for (int i = 0; i < parameterTypes.length; i++) {
+                    if(i == parameterTypes.length - 1){
+                        System.out.print(parameterTypes[i].getName() + " args_" + i);
+                        break;
+                    }
+                    System.out.print(parameterTypes[i].getName() + " args_" + i + ",");
+                }
+            }
+            System.out.print(")");
+
+            //6.抛出的异常
+            Class[] exceptionTypes = m.getExceptionTypes();
+            System.out.print(" exception: " + Objects.isNull(exceptionTypes) + " length: " +exceptionTypes.length);
+            /*System.out.print(" exception " + Objects.isNull(exceptionTypes));
+            if(!(Objects.isNull(exceptionTypes) && exceptionTypes.length == 0)){
+                System.out.print(" throws ");
+                for (int i = 0; i < exceptionTypes.length; i++) {
+                    if(i == exceptionTypes.length - 1){
+                        System.out.print(exceptionTypes[i].getName());
+                        break;
+                    }
+                    System.out.print(exceptionTypes[i].getName() + ",");
+                }
+            }*/
+            if(exceptionTypes.length > 0){
+                System.out.print(" throws ");
+                for (int i = 0; i < exceptionTypes.length; i++) {
+                    if(i == exceptionTypes.length - 1){
+                        System.out.print(exceptionTypes[i].getName());
+                        break;
+                    }
+                    System.out.print(exceptionTypes[i].getName() + ",");
+                }
+            }
+
+
+            System.out.println();
+        }
+
     }
 
 
