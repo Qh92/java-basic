1bfcafd08845b109fd06405d607099a7239d0ddd
Qh92 <771600967@qq.com>
2021-02-14 23:59:45 +0800

    feat:添加Optional实体类

---
+++ stream/src/main/java/com/qinh/entity/Boy.java
@@ -0,0 +1,32 @@
+package com.qinh.entity;
+
+/**
+ * @author Qh
+ * @version 1.0
+ * @date 2021-02-14-22:17
+ */
+public class Boy {
+    private Girl girl;
+
+    public Boy() {
+    }
+
+    public Boy(Girl girl) {
+        this.girl = girl;
+    }
+
+    public Girl getGirl() {
+        return girl;
+    }
+
+    public void setGirl(Girl girl) {
+        this.girl = girl;
+    }
+
+    @Override
+    public String toString() {
+        return "Boy{" +
+                "girl=" + girl +
+                '}';
+    }
+}
---
+++ stream/src/main/java/com/qinh/entity/Girl.java
@@ -0,0 +1,33 @@
+package com.qinh.entity;
+
+/**
+ * @author Qh
+ * @version 1.0
+ * @date 2021-02-14-22:17
+ */
+public class Girl {
+
+    private String name;
+
+    public Girl() {
+    }
+
+    public Girl(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Girl{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
