ee1a1dd23a037efff2f53ceacf9ad15d043a512f
Qh <771600967@qq.com>
2021-02-25 17:37:28 +0800

    fix:删除src

--- src/main/java/org/qinh/edu/annotationtest/Deprecated.java
+++
@@ -1,9 +0,0 @@
-package org.qinh.edu.annotationtest;
-
-import org.qinh.edu.annotation.Apple;
-
-public class Deprecated {
-    public static void main(String[] args) {
-        new Apple().getColor();
-    }
-}
