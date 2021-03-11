352c0ea8a0a17765589bc6baace65fa66d14627f
Qh <771600967@qq.com>
2021-03-11 18:00:53 +0800

    feat:添加对象深度clone方法

---
+++ io/src/main/java/com/qinh/io/exer/MyUtil.java
@@ -0,0 +1,123 @@
+package com.qinh.io.exer;
+
+import java.io.*;
+
+/**
+ * 如何实现对象克隆
+ *
+ * @author: Qh
+ * @version: 1.0
+ * @date: 2021/3/11 17:13
+ */
+public class MyUtil {
+    public static <T extends Serializable> T clone (T obj) throws IOException, ClassNotFoundException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(baos);
+        oos.writeObject(obj);
+
+        ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+        ObjectInputStream ois = new ObjectInputStream(bais);
+        return (T)ois.readObject();
+    }
+}
+
+class Person implements Serializable {
+    private static final long serialVersionUID = -9102017020286042305L;
+
+    private String name;    // 姓名
+    private int age;        // 年龄
+    private Car car;        // 座驾
+
+    public Person(String name, int age, Car car) {
+        this.name = name;
+        this.age = age;
+        this.car = car;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+
+    @Override
+    public String toString() {
+        return "Person [name=" + name + ", age=" + age + ", car=" + car + "]";
+    }
+
+}
+
+/**
+ * 小汽车类
+ * @author nnngu
+ *
+ */
+class Car implements Serializable {
+    private static final long serialVersionUID = -5713945027627603702L;
+
+    private String brand;       // 品牌
+    private int maxSpeed;       // 最高时速
+
+    public Car(String brand, int maxSpeed) {
+        this.brand = brand;
+        this.maxSpeed = maxSpeed;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    @Override
+    public String toString() {
+        return "Car [brand=" + brand + ", maxSpeed=" + maxSpeed + "]";
+    }
+
+}
+
+class CloneTest {
+
+    public static void main(String[] args) {
+        try {
+            Person p1 = new Person("郭靖", 33, new Car("Benz", 300));
+            Person p2 = MyUtil.clone(p1);   // 深度克隆
+            p2.getCar().setBrand("BYD");
+            // 修改克隆的Person对象p2关联的汽车对象的品牌属性
+            // 原来的Person对象p1关联的汽车不会受到任何影响
+            // 因为在克隆Person对象时其关联的汽车对象也被克隆了
+            System.out.println(p1);
+            System.out.println(p2);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
