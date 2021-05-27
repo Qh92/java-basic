6e25300f1cc49516e94157d5645d65e2ec4edaff
Qh92 <771600967@qq.com>
2021-05-28 01:46:22 +0800

    feat:添加原子测试代码

---
+++ thread/src/main/java/atomic/AtomicIntegerTest.java
@@ -0,0 +1,105 @@
+package atomic;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author Qh
+ * @version 1.0
+ * @date 2021-05-27-23:47
+ */
+public class AtomicIntegerTest {
+
+    private static final int THREADS_CONUT = 20;
+    public static int count = 0;
+
+    public static void increase() {
+        count++;
+    }
+
+    public static void main(String[] args) {
+        Thread[] threads = new Thread[THREADS_CONUT];
+        for (int i = 0; i < THREADS_CONUT; i++) {
+            threads[i] = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (int i = 0; i < 1000; i++) {
+                        increase();
+                    }
+                }
+            });
+            threads[i].start();
+        }
+
+        while (Thread.activeCount() > 1) {
+            Thread.yield();
+        }
+        System.out.println(count);
+    }
+}
+
+/**
+ * 1、保证变量在线程间可见，对volatile变量所有的写操作都能立即反应到其他线程中，换句话说，volatile变量在各个线程中是一致的（得益于java内存模型—"先行发生原则"）；
+ * 2、禁止指令的重排序优化；
+ *
+ * 测试结果和上面的一致，每次都是输出小于200000的数字
+ * 核心点在于java里的运算（比如自增）并不是原子性的
+ */
+class AtomicIntegerTest2 {
+
+    private static final int THREADS_CONUT = 20;
+    public static volatile int count = 0;
+
+    public static void increase() {
+        count++;
+    }
+
+    public static void main(String[] args) {
+        Thread[] threads = new Thread[THREADS_CONUT];
+        for (int i = 0; i < THREADS_CONUT; i++) {
+            threads[i] = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (int i = 0; i < 10000; i++) {
+                        increase();
+                    }
+                }
+            });
+            threads[i].start();
+        }
+
+        while (Thread.activeCount() > 1) {
+            Thread.yield();
+        }
+        System.out.println(count);
+    }
+}
+
+/**
+ * 结果每次都输出20000，程序输出了正确的结果，这都归功于AtomicInteger.incrementAndGet()方法的原子性。
+ */
+class AtomicIntegerTest3 {
+
+    private static final int THREADS_CONUT = 20;
+    public static AtomicInteger count = new AtomicInteger(0);
+
+    public static void increase() {
+        count.incrementAndGet();
+    }
+
+    public static void main(String[] args) {
+        Thread[] threads = new Thread[THREADS_CONUT];
+        for (int i = 0; i < THREADS_CONUT; i++) {
+            threads[i] = new Thread(() -> {
+                for (int j = 0; j < 10000; j++) {
+                    increase();
+                }
+            });
+            threads[i].start();
+        }
+
+        while (Thread.activeCount() > 1) {
+            Thread.yield();
+        }
+        System.out.println(count);
+    }
+}
