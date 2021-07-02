d04ce102426b38d74c695ccc11b633b9ed1f8eef
Qh <771600967@qq.com>
2021-07-02 17:02:28 +0800

    feat:添加闭锁测试代码

--- thread/src/main/java/senior/juc/lock/TestCountDownLatch.java
+++ thread/src/main/java/senior/juc/lock/TestCountDownLatch.java
@@ -4,6 +4,9 @@ import java.util.concurrent.CountDownLatch;
 
 /*
  * CountDownLatch ：闭锁，在完成某些运算是，只有其他所有线程的运算全部完成，当前运算才继续执行
+ * CountDownLatch主要有两个方法，当一个或多个线程调用await方法时，这些线程会阻塞。
+ * 其它线程调用countDown方法会将计数器减1(调用countDown方法的线程不会阻塞)，
+ * 当计数器的值变为0时，因await方法阻塞的线程会被唤醒，继续执行。
  */
 public class TestCountDownLatch {
 
