83379e2c81c3ad7bedee72d9d1199858fc4b675e
Qh <771600967@qq.com>
2021-12-23 09:47:10 +0800

    fix:填写新的测试方法

--- exception/src/main/java/com/qinh/exception/ReturnExceptionDemo.java
+++ exception/src/main/java/com/qinh/exception/ReturnExceptionDemo.java
@@ -24,6 +24,31 @@ public class ReturnExceptionDemo {
         }
     }
 
+    static int methodC() {
+        try {
+            System.out.println("进入方法C");
+            return 1;
+        }finally {
+            System.out.println("调用C方法的finally");
+            return 2;
+        }
+    }
+
+    public static int getInt() {
+        int a = 10;
+        try {
+            System.out.println(a / 0);
+            a = 20;
+        } catch (ArithmeticException e) {
+            a = 30;
+            return a;
+            /** return a 在程序执行到这一步的时候，这里不是return a 而是 return 30；这个返回 路径就形成了 * 但是呢，它发现后面还有finally，所以继续执行finally的内容，a=40 * 再次回到以前的路径,继续走return 30，形成返回路径之后，这里的a就不是a变量了，而是 常量30 */
+        } finally {
+            a = 40;
+        }
+        return a;
+    }
+
     public static void main(String[] args) {
         try {
             methodA();
@@ -34,5 +59,7 @@ public class ReturnExceptionDemo {
 
 
         methodB();
+        System.out.println(methodC());
+        System.out.println(getInt());
     }
 }
